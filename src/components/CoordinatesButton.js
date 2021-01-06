b8e2a8ab23eb408633a17b61b9604df1e6a78757
cep104 <candicelizabethpeters@gmail.com>
2021-01-06 10:28:55 -0600

    commit tree 089090760d23560473b23a77f54d9d0eef210eb9

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from "react";
+
+export default class CoordinatesButton extends Component {
+  handleOnClick = (event) => {
+    // event.persist();
+    // need to use event.persist to see what is inside of event
+    // console.log(event);
+    let cordArray = [event.clientX, event.clientY];
+    this.props.onReceiveCoordinates(cordArray);
+    //use this.props to pass down the function. and send back up the cordArray.
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleOnClick}>CButton</button>
+      </div>
+    );
+  }
+}
